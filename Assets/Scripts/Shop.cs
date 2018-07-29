c82a771fcbfe24dbc30443b5225f10e5b4fffc7e
matt_g <mgaut@uci.edu>
2018-07-28 23:41:02 -0700

    Added Shop

---
+++ Assets/Scripts/Shop.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Inventory))]
+public class Shop : MonoBehaviour {
+
+    public Inventory inventory { get; private set; }
+
+    // Shop sell to Player
+    public void SellItem(Inventory selling_to, ItemType type, int item_id, int price, int count = 1) {
+        // Cancel if inventory does not have item to sell
+        if (!inventory.HasItem(type, item_id, count)) {
+            return;
+        }
+        if (selling_to.TrySpendGold(price * count)) {
+            inventory.AddGold(price * count);
+            inventory.RemoveItem(type, item_id);
+
+            selling_to.AddItem(type, item_id);
+        }
+    }
+
+    // Shop buy from Player
+    public void BuyItem(Inventory buying_from, ItemType type, int item_id, int price, int count = 1) {
+        if (!buying_from.HasItem(type, item_id, count)) {
+            return;
+        }
+        if (inventory.TrySpendGold(price * count)) {
+            buying_from.AddGold(price * count);
+            buying_from.RemoveItem(type, item_id);
+
+            inventory.AddItem(type, item_id);
+        }
+    }
+
+    void Awake() {
+        inventory = GetComponent<Inventory>();    
+    }
+}
---
+++ Assets/Scripts/UI/ShopItemDisplay.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+
+public class ShopItemDisplay : MonoBehaviour {
+    [SerializeField] Button plus_one, minus_one, confirm;
+    [SerializeField] InputField transaction_count_input_field;
+
+    [SerializeField] Text item_name, for_sale_count_text, price_text, confirm_text;
+    [SerializeField] Image image;
+
+    int for_sale_count, price, transaction_count;
+
+    int price_limit;
+    bool price_limit_set;
+
+    public void SetName(string name) {
+        item_name.text = name;
+    }
+    public void SetItemCount(int count) {
+        if (count <= 0) {
+            return;
+        }
+        for_sale_count_text.text = "x " + count;
+        for_sale_count = count;
+
+        SetTransactionCount(1);
+    }
+
+    public void SetPrice(int price) {
+        if (price <= 0) {
+            return;
+        }
+        this.price = price;
+        price_text.text = (transaction_count * price) + " Gold";
+    }
+    public void SetPriceLimit(int limit) {
+        price_limit = limit;
+        price_limit_set = true;
+        CheckUnderLimit();
+    }
+    public void SetConfirmText(string text) {
+        confirm_text.text = text;
+    }
+
+    public void SetConfirmAction(UnityAction action) {
+        confirm.onClick.AddListener(action);
+    }
+
+    void Awake() {
+        plus_one.onClick.AddListener(() => AddOne());
+        minus_one.onClick.AddListener(() => SubtractOne());
+
+        transaction_count_input_field.onEndEdit.AddListener((string new_text) => SetTransactionCount(new_text));
+    }
+
+    void AddOne() {
+        SetTransactionCount(transaction_count + 1);
+    }
+
+    void SubtractOne() {
+        SetTransactionCount(transaction_count - 1);
+    }
+
+    void SetTransactionCount(int count) {
+        if (count <= 0) {
+            return;
+        }
+        if (count > for_sale_count) {
+            count = for_sale_count;
+        }
+        transaction_count = count;
+        transaction_count_input_field.text = transaction_count + "";
+
+        // Make sure buttons cant be clicked when they would have no effect
+        minus_one.interactable = (count != 1);
+        plus_one.interactable = (count != for_sale_count);
+
+        price_text.text = (count * price) + " Gold";
+        CheckUnderLimit();
+    }
+
+    void SetTransactionCount(string count_text) {
+        SetTransactionCount(int.Parse(count_text));
+    }
+
+    void CheckUnderLimit() {
+        confirm.interactable = (!price_limit_set || (price * transaction_count) <= price_limit);
+    }
+
+}

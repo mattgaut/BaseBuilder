df393425756f1b5f7d55386986804786372c00a5
matt_g <mgaut@uci.edu>
2018-08-13 20:31:59 -0700

    Added Skill

---
+++ Assets/Scripts/Account/Skill.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Skill : MonoBehaviour {
+
+    public string skill_name { get { return _skill_name; } }
+    public int level { get; private set; }
+    public int max_level { get { return _max_level; } }
+    public int required_points { get { return _required_points; } }
+
+    [SerializeField] string _skill_name;
+    [SerializeField] int _max_level;
+    [SerializeField] int _required_points;
+
+    public bool TryLevelSkill() {
+
+    }
+}

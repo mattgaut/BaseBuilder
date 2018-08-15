e275e27749a5b36875981517a9ecac7905f95120
matt_g <mgaut@uci.edu>
2018-08-14 21:55:15 -0700

    Made Skill Inherit from Scriptable Object

--- Assets/Scripts/Account/Skill.cs
+++ Assets/Scripts/Account/Skill.cs
@@ -2,19 +2,24 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Skill : MonoBehaviour {
+[CreateAssetMenu(fileName = "Skills", menuName = "Skills", order = 1)]
+public class Skill : ScriptableObject {
 
     public string skill_name { get { return _skill_name; } }
     public int level { get; private set; }
     public int max_level { get { return _max_level; } }
     public int required_points { get { return _required_points; } }
+    public Sprite sprite { get { return _sprite; } }
 
-    public Skill parent { get; private set; }
+    public Skill parent { get { return _parent; } }
 
     [SerializeField] bool require_parent_max_level;
     [SerializeField] string _skill_name;
     [SerializeField] int _max_level;
     [SerializeField] int _required_points;
+    [SerializeField] Sprite _sprite;
+
+    [SerializeField] Skill _parent;
 
     public bool TryLevelSkill(int total_system_points) {
         if (level >= max_level) {
@@ -35,6 +40,6 @@ public class Skill : MonoBehaviour {
     }
 
     public void SetParentSkill(Skill parent) {
-        this.parent = parent;
+        _parent = parent;
     }
 }

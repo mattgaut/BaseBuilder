523b5f2bdaf5595eaefda95b5900f5cef0ab4eb9
matt_g <mgaut@uci.edu>
2018-08-17 22:27:58 -0700

    Added Stats to player character

--- Assets/Scripts/PlayerStats.cs
+++ Assets/Scripts/PlayerStats.cs
@@ -4,14 +4,16 @@ using UnityEngine;
 
 public class PlayerStats : MonoBehaviour, IStats {
     [SerializeField] ResourceStat _health;
-    [SerializeField] Stat _speed;
+    [SerializeField] Stat _speed, _strength, _intelligence, _dexterity, _vitality;
 
     public ResourceStat health {
         get { return _health; }
     }
-    public Stat speed {
-        get { return _speed; }
-    }
+    public Stat speed { get { return _speed; } }
+    public Stat stength { get { return _strength; } }
+    public Stat intelligence { get { return _intelligence; } }
+    public Stat dexterity { get { return _dexterity; } }
+    public Stat vitality { get { return _vitality; } }
 
     void Awake() {
         _health.Init();

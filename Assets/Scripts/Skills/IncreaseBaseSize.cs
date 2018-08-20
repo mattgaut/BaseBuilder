71791b229055f86638990020f98d2c0bb1a84bdf
matt_g <mgaut@uci.edu>
2018-08-19 22:26:30 -0700

    Added create asset menu tag

--- Assets/Scripts/Skills/IncreaseBaseSize.cs
+++ Assets/Scripts/Skills/IncreaseBaseSize.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[CreateAssetMenu(fileName = "IncreaseBaseSizeSkill", menuName = "Skills/IncreaseBaseSize", order = 1)]
 public class IncreaseBaseSize : AccountSkill {
 
     [SerializeField] int[] base_size_bonus_at_level;

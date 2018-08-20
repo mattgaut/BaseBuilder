43dc3cd6bb704b3e28a12d5cf87722a6db2ce79b
matt_g <mgaut@uci.edu>
2018-08-19 20:54:09 -0700

    Created Apply Effects

--- Assets/Scripts/Skills/IncreaseBaseSize.cs
+++ Assets/Scripts/Skills/IncreaseBaseSize.cs
@@ -7,7 +7,11 @@ public class IncreaseBaseSize : AccountSkill {
     [SerializeField] int[] base_size_bonus_at_level;
 
     protected override void ApplyEffects() {
-        throw new System.NotImplementedException();
+        int bonus_base_size = 0;
+        if (base_size_bonus_at_level.Length < level) {
+             bonus_base_size = base_size_bonus_at_level[level - 1];
+        }
+        applied_to.AddBonusBaseSize(bonus_base_size);
     }
 
     protected override void RemoveEffects() {

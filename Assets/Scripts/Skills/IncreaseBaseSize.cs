34460ecf4f199872c9f56ad4a8096f579de5bb8d
matt_g <mgaut@uci.edu>
2018-08-19 20:51:31 -0700

    Created Increase Base Size Script

---
+++ Assets/Scripts/Skills/IncreaseBaseSize.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IncreaseBaseSize : AccountSkill {
+
+    [SerializeField] int[] base_size_bonus_at_level;
+
+    protected override void ApplyEffects() {
+        throw new System.NotImplementedException();
+    }
+
+    protected override void RemoveEffects() {
+        throw new System.NotImplementedException();
+    }
+}
